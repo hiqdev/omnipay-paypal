45dc0e037f97ce6b5d536fdde75ff2e26ff2fa04
Andrii Vasyliev <sol@hiqdev.com>
2015-10-23 12:29:37 +0000

    + validation

--- src/Merchant.php
+++ src/Merchant.php
@@ -15,10 +15,9 @@ class Merchant extends \hiqdev\php\merchant\Merchant
 {
     protected static $_defaults = [
         #'actionUrl'  => 'https://sandbox.paypal.com/cgi-bin/webscr',
-        'name'        => 'paypal',
-        'label'       => 'PayPal',
-        'actionUrl'   => 'https://www.paypal.com/cgi-bin/webscr',
-        'confirmText' => 'OK',
+        'name'      => 'paypal',
+        'label'     => 'PayPal',
+        'actionUrl' => 'https://www.paypal.com/cgi-bin/webscr',
     ];
 
     public function getInputs()
@@ -35,7 +34,36 @@ class Merchant extends \hiqdev\php\merchant\Merchant
         ];
     }
 
+    public function prepareFrom($data)
+    {
+        $from    = "$data[address_name]/$data[payer_email]";
+        $charset = strtoupper($data['charset']);
+        if ($charset !== 'UTF-8') {
+            $from = iconv($charset, 'UTF-8//IGNORE', $from);
+        }
+
+        return $from;
+    }
+
     public function validateConfirmation($data)
     {
+        if ($data['payment_status'] !== 'Completed') {
+            return 'Not Completed';
+        }
+        $raw_post_data = 'cmd=_notify-validate&' . file_get_contents('php://input');
+        $result        = static::curl($this->actionUrl, $raw_post_data);
+        if ($result !== 'VERIFIED') {
+            return "Not VERIFIED: $result";
+        }
+        $this->mset([
+            'from'     => $this->prepareFrom($data),
+            'txn'      => $data['txn_id'],
+            'currency' => strtolower($data['mc_currency']),
+            'sum'      => $data['payment_gross'],
+            'fee'      => $data['payment_fee'],
+            'time'     => $this->formatDatetime($data['payment_date']),
+        ]);
+
+        return;
     }
 }
